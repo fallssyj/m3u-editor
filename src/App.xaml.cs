3c344eb385c8fb7f02bbaec2b398bccb0c74cd08
fallssyj <falls_syj@qq.com>
2026-02-01 15:21:56 +0800

    Update App.xaml.cs

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using m3u_editor.ViewModels;
+using System.Linq;
+using System.Windows;
 
 namespace m3u_editor
 {
@@ -7,6 +9,22 @@ namespace m3u_editor
     /// </summary>
     public partial class App : Application
     {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = Current?.MainWindow as MainWindow;
+            if (window is null)
+            {
+                return;
+            }
+
+            var target = e.Args.FirstOrDefault(MainViewModel.IsSupportedPlaylistFile);
+            if (!string.IsNullOrWhiteSpace(target) && window.DataContext is MainViewModel viewModel)
+            {
+                await viewModel.LoadPlaylistFromPathAsync(target);
+            }
+        }
     }
 
 }
